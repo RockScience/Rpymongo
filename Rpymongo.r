10a94eaa32f98bb7ba14f5d466e9f958da6fb55c
RockScience <pourriels.xfm@gmail.com>
2013-10-30 19:18:12 +0800

    added several arguments and functions

--- Rpymongo.r
+++ Rpymongo.r
@@ -11,29 +11,67 @@ mongodb.connect=function(dbName, collection, host, port, verbose=FALSE){
 #mongodb.connect(dbName, collection, host, port)
 
 
-mongodb.insert=function(doc_or_docs,verbose=FALSE) {
+mongodb.insert=function(doc_or_docs, manipulate=TRUE, safe="None", check_keys=TRUE, continue_on_error=FALSE, verbose=FALSE) {
   if (verbose)
     print(paste("collection full name:", rPython::python.get("collection._Collection__full_name.encode('ascii')")))
+
+  # conversion of arguments to python format
+  L = formals(mongodb.insert)
+  L=lapply(L, FUN=function(x) if (class(x)=="logical") capitalize(tolower(x)) else x)
+  L=L[setdiff(names(L),"verbose")]
+  myArgs = paste(paste(paste(names(L),rep("=",length(L))),as.character(L)),collapse=", ") 
   
-  return(rPython::python.get(paste("collection.insert(",doc_or_docs,")",sep="")))
+  return(rPython::python.get(paste("collection.insert(",myArgs,")",sep="")))
 }
 
 
 
-mongodb.save=function(to_save,verbose=FALSE) {
+mongodb.save=function(to_save, manipulate=TRUE, safe="None", check_keys=TRUE, verbose=FALSE) {
   if (verbose)
     print(paste("collection full name:", rPython::python.get("collection._Collection__full_name.encode('ascii')")))
   
-  return(rPython::python.get(paste("collection.save(",to_save,")",sep="")))
+  # conversion of arguments to python format
+  L = formals(mongodb.save)
+  L=lapply(L, FUN=function(x) if (class(x)=="logical") capitalize(tolower(x)) else x)
+  L=L[setdiff(names(L),"verbose")]
+  myArgs = paste(paste(paste(names(L),rep("=",length(L))),as.character(L)),collapse=", ") 
+  
+  return(rPython::python.get(paste("collection.save(",myArgs,")",sep="")))
 }
 
 
 
-mongodb.find=function(spec="None", verbose=FALSE, as.data.frame=FALSE) {
+mongodb.find=function(spec="None", 
+                      fields="None", 
+                      skip=0, 
+                      limit=0, 
+                      timeout="True", 
+                      snapshot=FALSE, 
+                      tailable=FALSE, 
+                      sort="None", 
+                      max_scan="None",
+                      as_class="None",
+                      slave_okay=FALSE,
+                      await_data=FALSE,
+                      partial=FALSE, 
+                      manipulate=TRUE, 
+                      #read_preference="ReadPreference.PRIMARY", 
+                      read_preference="pymongo.read_preferences.ReadPreference.PRIMARY",
+                      exhaust=FALSE, 
+                      verbose=FALSE, 
+                      as.data.frame=FALSE) {
+  #browser()
+  
+  # conversion of arguments to python format
+  L = formals(mongodb.find)
+  L=lapply(L, FUN=function(x) if (class(x)=="logical") capitalize(tolower(x)) else x)
+  L=L[setdiff(names(L),"as.data.frame")]
+  myArgs = paste(paste(paste(names(L),rep("=",length(L))),as.character(L)),collapse=", ") 
+  
   if (verbose)
     print(paste("collection full name:", rPython::python.get("collection._Collection__full_name.encode('ascii')")))
 
-  rPython::python.exec(paste("r = collection.find(spec=",spec,")",sep=""))
+  rPython::python.exec(paste("r = collection.find(",myArgs,")",sep=""))
   
   # Reference here: http://api.mongodb.org/python/current/api/bson/json_util.html
   rPython::python.exec("from bson import Binary, Code")
@@ -45,16 +83,28 @@ mongodb.find=function(spec="None", verbose=FALSE, as.data.frame=FALSE) {
   else
     return(resultJSON)
 }
-#cat(mongodb.find(verbose=TRUE))
-#mongodb.find(verbose=TRUE,as.data.frame=TRUE)
+#cat(mongodb.find())
+#fromJSON(mongodb.find())
+#mongodb.find(as.data.frame=TRUE)
 
 
 mongodb.remove=function(spec_or_id) {
   return(invisible(rPython::python.get(paste("collection.remove(",spec_or_id,")",sep=""))))
-}
+}w
 #mongodb.remove(spec_or_id=3) # by _id
 #mongodb.remove(spec_or_id="{ 'name' : \"aNameToBeRemoved\"}") # by spec
 #mongodb.remove(spec_or_id="{ 'name' : 'CLX15 Comdty'}") # by spec
+
+
+mongodb.dropCollection=function(name_or_collection, prompt=TRUE) {
+  if (prompt){
+    message=paste("You are about to drop the following collection: '", rPython::python.get("collection._Collection__full_name.encode('ascii')"),"'.\nThis action is not reversible, do you still want to proceed? (y/n) ",sep="")
+    nb=readline(message)
+    if (!identical(nb,"y")) return(NULL)
+  }
+  
+  return(rPython::python.get(paste("collection.save(",name_or_collection,")",sep="")))
+}
 
 
 mongodb.create_index=function(key_or_list, cache_for=300) {
@@ -63,6 +113,12 @@ mongodb.create_index=function(key_or_list, cache_for=300) {
 }
 
 
-mongodb.closeConnection=function(){
-  
+mongodb.closeConnectionPool=function(){
+  #ends all connections to the connection pool
+  rPython::python.get("client.disconnect()")
+}
+
+mongodb.alive=function(){
+  #checks if a connection can be established with the specified host+port
+  rPython::python.get("client.alive()")
 }
